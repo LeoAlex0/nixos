a92b75a17907333bb69428f1244f031a04b8c66a
zLeoAlex <z.alex.leo@outlook.com>
2025-11-19 01:23:13 +0800

    feat: using system config network application

--- configuration.nix
+++ configuration.nix
@@ -116,7 +116,20 @@
     git
     sbctl # To manage SecureBoot keys
     lm_sensors # To monitor temperature`
+
+    # Network
+    sing-box
+    v2raya
+    v2ray
+    v2ray-domain-list-community
+    v2ray-geoip
+    n2n
   ];
+
+  programs.throne = {
+    enable = true;
+    tunMode.enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
